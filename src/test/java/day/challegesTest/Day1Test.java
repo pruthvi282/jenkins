7a703ed67b48ef421c375235c946a84cd6c134a2
pruthvi282 <pruthvi282@gmail.com>
2021-04-10 21:01:21 +0530

    first commit

---
+++ src/test/java/day/challegesTest/Day1Test.java
@@ -0,0 +1,45 @@
+package day.challegesTest;
+
+import java.time.zone.ZoneOffsetTransitionRule.TimeDefinition;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+/***
+ * 
+ * @author Pruthvi
+ *
+ */
+public class Day1Test {
+	
+	@Test
+	public void day1Test() throws Throwable{
+		String browser = System.getProperty("browser");
+		if(browser.equals("firefox")){
+		
+	WebDriver driver=new FirefoxDriver();
+	driver.get("https://www.worldometers.info/world-population/");
+	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	int count=1;
+	
+	System.out.println(driver.findElement(By.xpath("//span[@class='rts-counter' and @rel='current_population']")).getText());
+	
+	while(count<5){
+	System.out.println(driver.findElement(By.xpath("//span[@class='rts-counter' and @rel='current_population']")).getText());
+	System.out.println(driver.findElement(By.xpath("//span[@class='rts-counter' and @rel='births_today']")).getText());
+	System.out.println(driver.findElement(By.xpath("//span[@class='rts-counter' and @rel='absolute_growth']")).getText());
+	System.out.println("*****************************************************************");
+	Thread.sleep(1000);
+	count++;
+				}
+			}
+		else{
+			System.out.println("No browser is selected");
+		}	
+	}
+	}
